a588fee937792c72ecb5e6aed22ce5829bc3c4e7
Azrul Haifan <ahaifan@gmail.com>
2025-02-12 21:39:49 +0700

    When color resource updated ++ ++ Make sure to update product variant SKU and Name

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -2,12 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Color extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'code',
     ];
+
+    protected static function booted(): void
+    {
+        static::updated(function ($color) {
+            foreach ($color->productVariants as $variant) {
+                $variant->updateSkuAndName();
+            }
+        });
+    }
+
+    public function productVariants(): HasMany
+    {
+        return $this->hasMany(ProductVariant::class);
+    }
 }
