ac181ba2ca44076069bb86629a100f0461f2d396
Azrul Haifan <ahaifan@gmail.com>
2025-02-18 23:10:26 +0700

    Add basic stock in resource

---
+++ app/Filament/Resources/StockInResource/Pages/ListStockIns.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\StockInResource\Pages;
+
+use App\Filament\Resources\StockInResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListStockIns extends ListRecords
+{
+    protected static string $resource = StockInResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Add Stock'),
+        ];
+    }
+}
