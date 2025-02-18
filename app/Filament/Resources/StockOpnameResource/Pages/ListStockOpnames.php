d13ba6bc807a47a7d58ef52e78ba3afa96efbeb2
Azrul Haifan <ahaifan@gmail.com>
2025-02-18 21:30:59 +0700

    Add subheading on stock opname resource

--- app/Filament/Resources/StockOpnameResource/Pages/ListStockOpnames.php
+++ app/Filament/Resources/StockOpnameResource/Pages/ListStockOpnames.php
@@ -3,17 +3,21 @@
 namespace App\Filament\Resources\StockOpnameResource\Pages;
 
 use App\Filament\Resources\StockOpnameResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListStockOpnames extends ListRecords
 {
     protected static string $resource = StockOpnameResource::class;
 
+    public function getSubheading(): ?string
+    {
+        return 'Manage and track all stock adjustments through stock opname process';
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            \Filament\Actions\CreateAction::make(),
         ];
     }
 }
