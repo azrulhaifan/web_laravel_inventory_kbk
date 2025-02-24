bc5ed11a4b9e63e9caef71f278e3d43e3f883c05
Azrul Haifan <ahaifan@gmail.com>
2025-02-24 23:00:32 +0700

    Add url link to dashboard widget ++ also add critical product qty widget

--- app/Filament/Widgets/ProductStatsOverviewWidget.php
+++ app/Filament/Widgets/ProductStatsOverviewWidget.php
@@ -16,17 +16,33 @@ class ProductStatsOverviewWidget extends BaseWidget
             Stat::make('Total Products', ProductVariant::count())
                 ->description('Total active products')
                 ->descriptionIcon('heroicon-m-shopping-bag')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.apps.resources.product-variants.index')),
+
+            Stat::make('Low Stock Alert', ProductVariant::where('current_stock', '<=', 5)->count())
+                ->description('Products with stock <= 5')
+                ->descriptionIcon('heroicon-m-exclamation-triangle')
+                ->color('danger')
+                ->url(route('filament.apps.resources.product-variants.index', [
+                    'tableFilters[quantity][operator]' => '<=',
+                    'tableFilters[quantity][value]' => '5',
+                ])),
 
             Stat::make('Pending Stock In', StockIn::where('stock_in_status_id', 2)->count())
                 ->description('Waiting to be processed')
                 ->descriptionIcon('heroicon-m-arrow-down-circle')
-                ->color('warning'),
+                ->color('warning')
+                ->url(route('filament.apps.resources.stock-ins.index', [
+                    'tableFilters[stock_in_status_id]' => '2',
+                ])),
 
             Stat::make('Pending Stock Out', StockOut::where('stock_out_status_id', 2)->count())
                 ->description('Waiting to be processed')
                 ->descriptionIcon('heroicon-m-arrow-up-circle')
-                ->color('warning'),
+                ->color('warning')
+                ->url(route('filament.apps.resources.stock-outs.index', [
+                    'tableFilters[stock_out_status_id]' => '2',
+                ])),
         ];
     }
 }
